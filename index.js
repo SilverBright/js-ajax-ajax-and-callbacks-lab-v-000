45907938f18c7a9321743d9490dbb3506e716cd9
Barbara Washington <barbara.m.washington@gmail.com>
2019-02-04 23:45:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 $(document).ready(function (){
 });
 
-function searchRepositories(){
-  const searchTerms = $('#searchTerms').val();
-  const uri = 'https://api.github.com/search/repositories?q=' + searchTerms
-  $.get(uri, function(data) {
+function searchRepositories() {
+    const searchTerms = $('#searchTerms').val();
+    const uri = 'https://api.github.com/search/repositories?q=' + searchTerms
+    $.get(uri, function(data) {
         console.log(data.items)
         $('#results').html(renderSearchResults(data.items))
     }).fail(function(error) {displayError(error)})
